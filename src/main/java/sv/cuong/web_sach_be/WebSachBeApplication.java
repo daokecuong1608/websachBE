afb68e4f2dbaedfdedc31ef1b88f7a155148cee4
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-09-15 01:14:36 +0700

    first commit

---
+++ src/main/java/sv/cuong/web_sach_be/WebSachBeApplication.java
@@ -0,0 +1,13 @@
+package sv.cuong.web_sach_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSachBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSachBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/cuong/web_sach_be/WebSachBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.cuong.web_sach_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSachBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
